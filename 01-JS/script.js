6ac086fccd05826adec388ccb56f26b00458848f
DanielLight-programmer <danielsfpereira934@gmail.com>
2026-08-27 22:04:52 -0300

    add exercicios

---
+++ 01-JS/script.js
@@ -0,0 +1,55 @@
+// alert("Eu sou o script no arquivo.js ")
+/////////////////////////////////////////////////
+//  Problema  exemplo Ler duas notas e calcular a média
+
+// infos e variáveis
+
+// let nota1, nota2
+// let media
+//  // ler dados
+
+//   nota1 = prompt('Digite a primeira nota: ')// lido e armazenado como texto
+//   Number(nota1)// coverte texto para número
+
+//   nota2 = Number(prompt('Digite a segunta nota: '))//lê, converte para número e armazenado
+// // Processar
+
+// media = (nota1 + nota2) /2
+// // saída
+
+// alert(' Média:  ' + media)
+
+//////////////////////////////////////////
+
+let vitorias, empates
+let pontos
+
+ //entrada de dados
+ vitorias = Number(prompt("Digite o número de vitórias: "))
+ empates = Number(prompt("Digite o número de empates: "))
+
+ // processos
+pontos = vitorias*3 + empates
+ // saídas
+
+  alert('Pontos do seu time:' + pontos)
+
+  ////////////////////////////////////////
+
+// infos e variaveis
+  let pessoas 
+  let carne, cerveja, agua, refri
+
+  // entrada de dados
+pessoas + Number(prompt('Digite o número de pessos que vai para o churrasco: '))
+
+ // processos 
+  carne  = pessoas 
+  cerveja = pessoas 
+  agua = pessoas 
+  pessoas =  pessoas 
+ // saídas
+ 
+
+
+
