359cb51eb3333ea1fe9606a69fbd6b3b131ddb0f
DanielLight-programmer <danielsfpereira934@gmail.com>
2026-09-17 22:05:31 -0300

    repetiçõesContinuaçõesdosExercícios

--- 03-repetições/script.js
+++ 03-repetições/script.js
@@ -1,7 +1,38 @@
 let resultado = document.getElementById('resultado')
 
+function parte3ex78(){
+    resultado.innerHTML = 'Parte3-ex7.8 <br> '
+
+}
+function parte1ex7(){
+   resultado.innerHTML = 'Parte1- ex7 <br> '
+
+
+    }
+
+function parte1ex6(){
+    resultado.innerHTML = 'Parte1- ex6 <br> '
+    for(let i = 1; i<=20;i++){
+        if(i%2 != 0){
+            resultado.innerHTML += '<p class="numeros">' + i + '</p>'
+        }
+    }
+
+}
+
+function parte1ex4(){
+    resultado.innerHTML = 'Parte 1- ex4<br>'
+
+    let i = 1
+    while(i<=10){
+        let multiplicacao = i * 5
+        resultado.innerHTML += '5 x ' + i + ' = ' + multiplicacao + '<br>'
+        i++
+    }
+}
+
 function parte1ex3(){
-    resultado.innerHTML = 'Parte 1, ex3<br>'
+    resultado.innerHTML = 'Parte 1- ex3<br>'
     let soma = 0
     let i = 1
     while(i<=100){
@@ -15,7 +46,7 @@ function parte1ex3(){
 
 function parte1ex2(){
 
-    resultado.innerHTML = 'Parte 1, ex2<br>'
+    resultado.innerHTML = 'Parte 1- ex2<br>'
 
     let i = 10
     while(i >= 0){
@@ -31,7 +62,7 @@ function parte1ex2(){
 }
 
 function parte1ex1(){
-    document.getElementById('resultado').innerHTML = 'Parte 1, ex1<br>'
+    document.getElementById('resultado').innerHTML = 'Parte 1- ex1<br>'
 //ex1
     let i = 0
     while(i<10){
